9638d9b17ecdfe41d088e1e96a25c82c24065b59
Vitaliy Meshchaninov <glukki.spb.ru@gmail.com>
2016-02-28 02:04:57 +0300

    test sandbox

---
+++ test/sandbox.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * Sandbox declaration
+ */
+
+import fs from 'fs';
+import path from 'path';
+import sinon from 'sinon';
+
+
+// root hooks that are called before and after every test case
+beforeEach(function () {
+    this.import = importSource;
+    this.sinon = sinon.sandbox.create();
+});
+afterEach(function () {
+    this.sinon.restore();
+});
+
+
+// global function to shortcut source files loading
+function importSource(location) {
+    if (typeof location !== 'string') {
+        throw new Error(`Provide path relative to 'src/' directory`);
+    }
+
+    let module = require(path.join('../src/', location));
+    // handle es6 and node modules
+    return module.default || module;
+}
