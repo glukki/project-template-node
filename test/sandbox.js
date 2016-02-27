ee46d330f233bf22845e3428f60377db33d5f844
Vitaliy Meshchaninov <glukki.spb.ru@gmail.com>
2016-02-28 02:30:04 +0300

    format

--- test/sandbox.js
+++ test/sandbox.js
@@ -4,28 +4,27 @@
  * Sandbox declaration
  */
 
-import fs from 'fs';
 import path from 'path';
 import sinon from 'sinon';
 
 
 // root hooks that are called before and after every test case
 beforeEach(function () {
-    this.import = importSource;
-    this.sinon = sinon.sandbox.create();
+  this.import = importSource;
+  this.sinon = sinon.sandbox.create();
 });
 afterEach(function () {
-    this.sinon.restore();
+  this.sinon.restore();
 });
 
 
 // global function to shortcut source files loading
 function importSource(location) {
-    if (typeof location !== 'string') {
-        throw new Error(`Provide path relative to 'src/' directory`);
-    }
+  if (typeof location !== 'string') {
+    throw new Error(`Provide path relative to 'src/' directory`);
+  }
 
-    let module = require(path.join('../src/', location));
-    // handle es6 and node modules
-    return module.default || module;
+  let module = require(path.join('../src/', location));
+  // handle es6 and node modules
+  return module.default || module;
 }
